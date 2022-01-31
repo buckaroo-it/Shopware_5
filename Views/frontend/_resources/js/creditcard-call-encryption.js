2ad37adb108d0f2d5581141ed8eeb2f39e34de72
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-01-31 10:49:39 +0100

    check values on the currently selected payment option

--- Views/frontend/_resources/js/creditcard-call-encryption.js
+++ Views/frontend/_resources/js/creditcard-call-encryption.js
@@ -1,35 +1,37 @@
 $(function() {
-    $('#shippingPaymentForm').submit(submit);   
+    $('.content-main--inner').on('click','[form="shippingPaymentForm"], #shippingPaymentForm input[type="submit"]',submit);   
 });
 var submit = function(e) {
     e.preventDefault();
-    var cardNumber = $(".cardNumber").val();
-    var cvc = $(".cvc").val();
-    var cardHolderName = $(".cardHolderName").val();
-    var expirationYear = $(".expirationYear").val();
-    var expirationMonth = $(".expirationMonth").val();
-    var cardNumberValid = BuckarooClientSideEncryption.V001.validateCardNumber(cardNumber);
-    var cvcValid = BuckarooClientSideEncryption.V001.validateCvc(cvc);
-    var cardHolderNameValid = BuckarooClientSideEncryption.V001.validateCardholderName(cardHolderName);
-    var expirationYearValid = BuckarooClientSideEncryption.V001.validateYear(expirationYear);
-    var expirationMonthValid = BuckarooClientSideEncryption.V001.validateMonth(expirationMonth);
-    if (cardNumberValid && cvcValid && cardHolderNameValid && expirationYearValid && expirationMonthValid) {
-        getEncryptedData(cardNumber, expirationYear, expirationMonth, cvc, cardHolderName);
+    var rootBlock = window.jQuery('.payment--method input[type="radio"]:checked').closest('.payment--method');
+    if (rootBlock.find('.cardNumber').length) {
+        var cardNumber = rootBlock.find('.cardNumber').val();
+        var cvc = rootBlock.find('.cvc').val();
+        var cardHolderName = rootBlock.find('.cardHolderName').val();
+        var expirationYear = rootBlock.find('.expirationYear').val();
+        var expirationMonth = rootBlock.find('.expirationMonth').val();
+        var cardNumberValid = BuckarooClientSideEncryption.V001.validateCardNumber(cardNumber);
+        var cvcValid = BuckarooClientSideEncryption.V001.validateCvc(cvc);
+        var cardHolderNameValid = BuckarooClientSideEncryption.V001.validateCardholderName(cardHolderName);
+        var expirationYearValid = BuckarooClientSideEncryption.V001.validateYear(expirationYear);
+        var expirationMonthValid = BuckarooClientSideEncryption.V001.validateMonth(expirationMonth);
+        if (cardNumberValid && cvcValid && cardHolderNameValid && expirationYearValid && expirationMonthValid) {
+            getEncryptedData(cardNumber, expirationYear, expirationMonth, cvc, cardHolderName, rootBlock);
+        } else {
+            $('#shippingPaymentForm').submit();
+        }
     } else {
-        $('#shippingPaymentForm').off("submit");
-        $("#shippingPaymentForm").trigger('submit');        
+        $('#shippingPaymentForm').submit();
     }
-    
 }
-var getEncryptedData = function(cardNumber, year, month, cvc, cardholder) {
+var getEncryptedData = function(cardNumber, year, month, cvc, cardholder, rootBlock) {
     BuckarooClientSideEncryption.V001.encryptCardData(cardNumber,
         year,
         month,
         cvc,
         cardholder,
         function(encryptedCardData) {
-            $(".encryptedCardData").val(encryptedCardData);
-            $('#shippingPaymentForm').off("submit");
-            $("#shippingPaymentForm").trigger('submit');
+            rootBlock.find(".encryptedCardData").val(encryptedCardData);
+            $('#shippingPaymentForm').submit();
         });
 }
